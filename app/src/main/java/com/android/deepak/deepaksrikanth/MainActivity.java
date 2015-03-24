788686bd43e4dd83cc11eb5cae9c725d73f2c586
srikanth <srikanth.android@hotmail.com>
2015-03-24 08:53:01 +0530

    changed MainActivity.java

--- app/src/main/java/com/android/deepak/deepaksrikanth/MainActivity.java
+++ app/src/main/java/com/android/deepak/deepaksrikanth/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Toast done by srikanth",5000);
     }
 
 
